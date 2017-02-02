72d9cca781332792d5d397d850d17f3511fef5be
rarilurelo <rarilurelo@gmail.com>
2017-02-02 11:11:35 +0900

    add test code

--- run_a3c.py
+++ run_a3c.py
@@ -1,3 +1,4 @@
+import os
 import numpy as np
 from skimage.color import rgb2gray
 from skimage.transform import resize
@@ -20,6 +21,7 @@ def train(rank, global_policy, local_policy, optimizer, env, global_t, args):
     o = env.reset()
     step = 0
     sum_rewards = 0
+    max_sum_rewards = 0
     while global_t[0] < args.epoch:
         local_policy.sync(global_policy)
         observations = []
@@ -49,6 +51,14 @@ def train(rank, global_policy, local_policy, optimizer, env, global_t, args):
                     print('----------------------------------')
                     print('total reward of the episode:', sum_rewards)
                     print('----------------------------------')
+                    if args.save_mode == 'all':
+                        torch.save(local_policy, os.path.join(args.log_dir, args.save_name+"_{}.pkl".format(global_t[0])))
+                    elif args.save_mode == 'last':
+                        torch.save(local_policy, os.path.join(args.log_dir, args.save_name+'.pkl'))
+                    elif args.save_mode == 'max':
+                        if max_sum_rewards < sum_rewards:
+                            torch.save(local_policy, os.path.join(args.log_dir, args.save_name+'.pkl'))
+                            max_sum_rewards = sum_rewards
                     sum_rewards = 0
                     step = 0
                 break
@@ -89,7 +99,7 @@ if __name__ == '__main__':
                         help='render the environment')
     parser.add_argument('--monitor', action='store_true',
                         help='save the rendered video')
-    parser.add_argument('--log_dir', type=str, default='./movie',
+    parser.add_argument('--log_dir', type=str, default='./log_dir',
                         help='save dir')
     parser.add_argument('--epoch', type=int, default=10000000, metavar='N',
                         help='training epoch number')
@@ -109,7 +119,13 @@ if __name__ == '__main__':
                         help='number of processes')
     parser.add_argument('--eps', type=float, default=0.1, metavar='E',
                         help='epsilon minimum log or std')
+    parser.add_argument('--save_name', type=str, default='exp', metavar='N',
+                        help='define save name')
+    parser.add_argument('--save_mode', type=str, default='max', metavar='S',
+                        help='save mode. all or last or max')
     args = parser.parse_args()
+    if not os.path.exists(args.log_dir):
+        os.mkdir(args.log_dir)
 
     env = gym.make(args.env)
     if args.monitor:
@@ -120,7 +136,7 @@ if __name__ == '__main__':
     
     global_policy = Policy(env.action_space.n, env.observation_space.shape[0], args.frame_num)
     global_policy.share_memory()
-    local_policy = Policy(env.action_space.n, env.observation_space.shape[0])
+    local_policy = Policy(env.action_space.n, env.observation_space.shape[0], args.frame_num)
     
     optimizer = AsyncRMSprop(global_policy.parameters(), local_policy.parameters(), lr=args.lr, eps=args.eps)
     
---
+++ test_a3c.py
@@ -0,0 +1,72 @@
+import os
+import numpy as np
+from skimage.color import rgb2gray
+from skimage.transform import resize
+import argparse
+import gym
+from gym import wrappers
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.autograd as autograd
+from torch.autograd import Variable
+import torch.multiprocessing as mp
+
+from async_rmsprop import AsyncRMSprop
+from policy import Policy
+from wrapper_env import StackEnv
+
+
+def test(policy, args):
+    step = 0
+    sum_rewards = 0
+    while step < args.num_rollout:
+        done = False
+        o = env.reset()
+        while not done:
+            p, v = policy(o)
+            a = p.multinomial()
+            o, r, done, _ = env.step(a.data.squeeze()[0])
+            sum_rewards += r
+            if args.render:
+                env.render()
+        print('----------------------------------')
+        print('total reward of the episode:', sum_rewards)
+        print('----------------------------------')
+        sum_rewards = 0
+        step += 1
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='PyTorch a3c load model and test')
+    parser.add_argument('--seed', type=int, default=543, metavar='N',
+                        help='random seed (default: 1)')
+    parser.add_argument('--render', action='store_true',
+                        help='render the environment')
+    parser.add_argument('--monitor', action='store_true',
+                        help='save the rendered video')
+    parser.add_argument('--log_dir', type=str, default='./log_dir',
+                        help='save dir')
+    parser.add_argument('--frame_num', type=int, default=4, metavar='N',
+                        help='number of frames you use as observation')
+    parser.add_argument('--env', type=str, default='CartPole-v0',
+                        help='Environment')
+    parser.add_argument('--model_path', type=str, default='./log_dir/exp.pkl', metavar='S',
+                        help='path of saved model')
+    parser.add_argument('--num_rollout', type=int, default=6, metavar='N',
+                        help='number of rollout')
+    args = parser.parse_args()
+    if not os.path.exists(args.log_dir):
+        os.mkdir(args.log_dir)
+
+    env = gym.make(args.env)
+    if args.monitor:
+        env = wrappers.Monitor(env, args.log_dir, force=True)
+    env = StackEnv(env, args.frame_num)
+    env.seed(args.seed)
+    torch.manual_seed(args.seed)
+
+    policy = torch.load(args.model_path)
+
+    test(policy, args)
+
